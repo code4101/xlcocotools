d1efb78581f05eb541afd3ccd23eb8eaa9aad139
code4101 <877362867@qq.com>
2021-06-07 17:50:34 +0800

    v0.0.2 我的更改

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+pip install xlcocotools
+```
+
+官方原项目：[cocodataset/cocoapi: COCO API](https://github.com/cocodataset/cocoapi)
+
+我的修改：
+
+1. 增加"prt"参数，可以选择是否输出中间结果
+2. 兼容win、linux的安装
+
--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
-from setuptools import setup, Extension
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : 陈坤泽
+# @Email  : 877362867@qq.com
+# @Date   : 2020/11/26
+
+
+"""
+安装方法： python setup.py build_ext develop
+"""
+
+from setuptools import setup, find_packages, Extension
+import io
 import numpy as np
 import sys
 
@@ -14,22 +26,30 @@ else:
 
 ext_modules = [
     Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs=[np.get_include(), '../common'],
+        'xlcocotools._mask',
+        sources=['xlcocotools/common/maskApi.c', 'xlcocotools/_mask.pyx'],
+        include_dirs=[np.get_include(), 'xlcocotools/common'],
         extra_compile_args=extra_compile_args
     )
 ]
 
 setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir={'pycocotools': 'pycocotools'},
-    install_requires=[
-        'setuptools>=18.0',
-        'cython>=0.27.3',
-        'matplotlib>=2.1.0'
+    name='xlcocotools',  # pip 安装时用的名字
+    version='0.0.2',  # 当前版本，每次更新上传到pypi都需要修改
+    author='code4101',
+    author_email='877362867@qq.com',
+    url='https://github.com/XLPRUtils/xlcocotools',
+    keywords='xlcocotools',
+    description='pycocotools修改版本',
+    long_description=io.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',  # 大部分功能都是跨平台的
     ],
-    version='2.0',
+    python_requires='>=3.6',  # 我的项目大量使用f字符串
+    install_requires=open('requirements.txt').readlines(),
     ext_modules=ext_modules
 )
